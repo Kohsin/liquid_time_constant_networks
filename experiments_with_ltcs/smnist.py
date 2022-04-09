a11e6df22611aece3fa7929bb4094098fb09697f
Kohsin <102904030+Kohsin@users.noreply.github.com>
2022-04-10 02:10:16 +0800

    Update smnist.py

--- experiments_with_ltcs/smnist.py
+++ experiments_with_ltcs/smnist.py
@@ -58,7 +58,7 @@ class SMnistModel:
     def __init__(self,model_type,model_size,learning_rate = 0.001):
         self.model_type = model_type
         self.constrain_op = None
-        self.x = tf.placeholder(dtype=tf.float32,shape=[28,None,28])
+        self.x = tf.placeholder(dtype=tf.float32,shape=[32,None,96])
         print("self.x   " ,self.x.shape)
         self.target_y = tf.placeholder(dtype=tf.int32,shape=[None])
         print("self.target_y   " ,self.target_y.shape)
